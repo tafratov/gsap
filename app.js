a273a877ce3ec837a2717604f9132c8e79dce170
Login <mail@gmail.com>
2024-01-03 15:03:18 +0300

    2

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ let firstName = 'Artem'
 age = '31'
 city = 'Moscow'
 hobby = 'Fragrances, fashion & music'
-console.log('Hello', firstName)
+console.log('Hello', firstName, 'You are', age, 'years old.', 'Your hobbies:', hobby)
