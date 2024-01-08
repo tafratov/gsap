facce62fbe4ef791dfd2b95aa54d927b2948b87f
Login <mail@gmail.com>
2024-01-09 00:28:03 +0300

    commit

--- lesson-2.4/lesson-2.4.js
+++ lesson-2.4/lesson-2.4.js
@@ -1,8 +1,19 @@
-const titleProject = 'Webflow+jsap'
-let screensValue = 'анимация, работа с кодом, 3D'
-let screenPrice = 800
-let percentage = 20
-let fullPrice = 400
-let responsive = true
+// const titleProject = 'Webflow+jsap'
+// let screensValue = 'анимация, работа с кодом, 3D'
+// let screenPrice = 800
+// let percentage = 20
+// let fullPrice = 400
+// let responsive = true
 
-console.log(titleProject);
+// let x = 11;
+
+// if (x > 10) {
+//     console.log("Условие верно");
+// } else {
+//     console.log("Условие не верно");
+// }
+
+let a = 5;
+let b = '10'
+
+console.log(a + b)
