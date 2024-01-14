fadab4b703894a5ce909e85a6ef08840ffec6af7
Login <mail@gmail.com>
2024-01-14 23:38:10 +0300

    save

--- lesson-2.4/lesson-2.4.js
+++ lesson-2.4/lesson-2.4.js
@@ -1,19 +1,25 @@
-// const titleProject = 'Webflow+jsap'
-// let screensValue = 'анимация, работа с кодом, 3D'
-// let screenPrice = 800
-// let percentage = 20
-// let fullPrice = 400
-// let responsive = true
+let titleProject = 'Webflow+jsap'
+let screensValue = 'анимация, работа с кодом, 3D'
+let screenPrice = 800
+let percentage = 20
+let fullPrice = 400
+let responsive = true
 
-// let x = 11;
+// // let x = 11;
 
-// if (x > 10) {
-//     console.log("Условие верно");
-// } else {
-//     console.log("Условие не верно");
-// }
+// // if (x > 10) {
+// //     console.log("Условие верно");
+// // } else {
+// //     console.log("Условие не верно");
+// // }
 
-let a = 5;
-let b = '10'
+let titleProject = prompt('Название проекта?')
+let screensValue = prompt('анимация, работа с кодом, 3D')
+let responsive = prompt('нужен ли респонсивный сайт?')
 
-console.log(a + b)
+let service1 = prompt('Какой сервис нужен?')
+let servicePrice1 = prompt('Сколько это будет стоить?')
+let service2 = prompt('Какой еще сервис тебе нужен?')
+let servicePrice2 = ('Сколько будет стоить этот второй сервис?')
+let fullPrice = screenPrice + servicePrice1 + servicePrice2
+let servicePercentPrice = fullPrice - 10 %;
