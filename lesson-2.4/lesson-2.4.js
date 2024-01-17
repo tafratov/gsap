7307193478bebcf98b57d185b5e91e757309d763
Login <mail@gmail.com>
2024-01-18 01:04:32 +0300

    save

--- lesson-2.4/lesson-2.4.js
+++ lesson-2.4/lesson-2.4.js
@@ -1,25 +1,34 @@
-let titleProject = 'Webflow+jsap'
-let screensValue = 'анимация, работа с кодом, 3D'
-let screenPrice = 800
-let percentage = 20
-let fullPrice = 400
-let responsive = true
+// let titleProject = 'Webflow+jsap'
+// let screensValue = 'анимация, работа с кодом, 3D'
+// let screenPrice = 800
+// let percentage = 20
+// let fullPrice = 400
+// let responsive = true
 
-// // let x = 11;
+// // // let x = 11;
 
-// // if (x > 10) {
-// //     console.log("Условие верно");
-// // } else {
-// //     console.log("Условие не верно");
-// // }
+// // // if (x > 10) {
+// // //     console.log("Условие верно");
+// // // } else {
+// // //     console.log("Условие не верно");
+// // // }
 
-let titleProject = prompt('Название проекта?')
-let screensValue = prompt('анимация, работа с кодом, 3D')
-let responsive = prompt('нужен ли респонсивный сайт?')
-
+let titleProject = prompt('Название проекта?', 'Webflow+jsap')
+let screensValue = prompt('Типы экранов?', 'Шаблонные, с уникальным дизайном, с анимациями')
+let screenPrice = 10000
 let service1 = prompt('Какой сервис нужен?')
-let servicePrice1 = prompt('Сколько это будет стоить?')
-let service2 = prompt('Какой еще сервис тебе нужен?')
-let servicePrice2 = ('Сколько будет стоить этот второй сервис?')
+let servicePrice1 = +prompt('Сколько это будет стоить?')
+let service2 = prompt('Какой сервис еще нужен?')
+let servicePrice2 = +prompt('Сколько будет стоить этот второй сервис?')
+let responsive = prompt('Нужен ли респонсивный сайт?', 'Да')
 let fullPrice = screenPrice + servicePrice1 + servicePrice2
-let servicePercentPrice = fullPrice - 10 %;
+
+if (50000 => fullPrice) {
+    console.log('делаем скидку в 10%');
+} else if (20000 <= fullPrice <= 40000) {
+    console.log('сделаем скидку 5%');
+} else if (0 <= fullPrice <= 20000) {
+    console.log('скидка не предусмотрена');
+} else (fullPrice < 0) {
+    console.log('Что то пошло не так');
+}
