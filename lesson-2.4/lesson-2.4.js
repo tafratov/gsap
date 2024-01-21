7d404fe38ed3c3a18e7febbca2efb7306fa6aaa3
Login <mail@gmail.com>
2024-01-21 14:45:21 +0300

    save

--- lesson-2.4/lesson-2.4.js
+++ lesson-2.4/lesson-2.4.js
@@ -14,21 +14,24 @@
 // // // }
 
 let titleProject = prompt('Название проекта?', 'Webflow+jsap')
-let screensValue = prompt('Типы экранов?', 'Шаблонные, с уникальным дизайном, с анимациями')
-let screenPrice = 10000
+let screensValue = prompt('Какой тип экрана?', 'Шаблонные, с уникальным дизайном, с анимациями')
+let responsive = prompt('Нужен ли отзывчивы (responsive) сайт?')
 let service1 = prompt('Какой сервис нужен?')
 let servicePrice1 = +prompt('Сколько это будет стоить?')
 let service2 = prompt('Какой сервис еще нужен?')
 let servicePrice2 = +prompt('Сколько будет стоить этот второй сервис?')
-let responsive = prompt('Нужен ли респонсивный сайт?', 'Да')
-let fullPrice = screenPrice + servicePrice1 + servicePrice2
+let fullPrice = servicePrice1 + servicePrice2
+let percent = 10 //процент подрядчику
+let servicePercentPrice = fullPrice * (100 - percent) / 100;
+servicePercentPrice = Math.round(servicePercentPrice * 100) / 100;
+console.log(servicePercentPrice);
 
-if (fullPrice >= 50000) {
+if (servicePercentPrice >= 50000) {
     console.log('делаем скидку в 10%');
-} else if (fullPrice >= 20000 && fullPrice <= 40000) {
+} else if (servicePercentPrice >= 20000 && servicePercentPrice <= 40000) {
     console.log('сделаем скидку 5%');
-} else if (fullPrice >= 0 && fullPrice <= 20000) {
+} else if (servicePercentPrice >= 0 && servicePercentPrice <= 20000) {
     console.log('скидка не предусмотрена');
-} else (fullPrice < 0) {
+} else {
     console.log('Что то пошло не так');
 }
