c4a5fbc150887561fbe95dfcfd08e4f5e2261430
Login <mail@gmail.com>
2024-01-23 00:37:02 +0300

    save

--- lesson-2.4/lesson-2.4.js
+++ lesson-2.4/lesson-2.4.js
@@ -3,18 +3,36 @@
 // let screenPrice = 800
 // let percentage = 20
 // let fullPrice = 400
-let responsive = true
+// let responsive = true
+
+
+let screenPrice = 10000
+let percent = 10 //процент подрядчику
 
 let titleProject = prompt('Название проекта?', 'Webflow+jsap')
-let screenPrice = 10000
+console.log(titleProject);
+
 let screensValue = prompt('Какой тип экрана?', 'Шаблонные, с уникальным дизайном, с анимациями')
+console.log(screensValue);
+
 let responsive = prompt('Нужен ли отзывчивы (responsive) сайт?')
+console.log(responsive);
+
 let service1 = prompt('Какой сервис нужен?')
+console.log(service1);
+
 let servicePrice1 = +prompt('Сколько это будет стоить?')
+console.log(servicePrice1);
+
 let service2 = prompt('Какой сервис еще нужен?')
+console.log(service2);
+
 let servicePrice2 = +prompt('Сколько будет стоить этот второй сервис?')
+console.log(servicePrice2);
+
 let fullPrice = screenPrice + servicePrice1 + servicePrice2
-let percent = 10 //процент подрядчику
+console.log(fullPrice);
+
 let servicePercentPrice = fullPrice * (100 - percent) / 100;
 servicePercentPrice = Math.round(servicePercentPrice * 100) / 100;
 console.log(servicePercentPrice);
