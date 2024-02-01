708940aadfff1173614cc76858b369625e081f39
Login <mail@gmail.com>
2024-02-01 23:50:40 +0300

    save

--- lesson-2.5/lesson-2.5.js
+++ lesson-2.5/lesson-2.5.js
@@ -2,32 +2,32 @@ let screenPrice = 10000
 let percent = 10 //процент подрядчику
 
 let titleProject = prompt('Название проекта?', 'Webflow+jsap')
-console.log(titleProject);
+console.log('Название проекта:', titleProject);
 
 let screensValue = prompt('Какой тип экрана?', 'Шаблонные, с уникальным дизайном, с анимациями')
-console.log(screensValue);
+console.log('Тип проекта:', screensValue);
 
 let responsive = prompt('Нужен ли отзывчивы (responsive) сайт?')
-console.log(responsive);
+console.log('Отзывчивость дизайна:', responsive);
 
 let service1 = prompt('Какой сервис нужен?')
-console.log(service1);
+console.log('Первый сервис:', service1);
 
 let servicePrice1 = +prompt('Сколько это будет стоить?')
-console.log(servicePrice1);
+console.log('Цена за первый сервис:', servicePrice1);
 
 let service2 = prompt('Какой сервис еще нужен?')
-console.log(service2);
+console.log('Второй сервис:', service2);
 
 let servicePrice2 = +prompt('Сколько будет стоить этот второй сервис?')
-console.log(servicePrice2);
+console.log('Цена за второй сервис:', servicePrice2);
 
 let fullPrice = screenPrice + servicePrice1 + servicePrice2
-console.log(fullPrice);
+console.log('Общая цена:', fullPrice);
 
 let servicePercentPrice = fullPrice * (100 - percent) / 100;
 servicePercentPrice = Math.round(servicePercentPrice * 100) / 100;
-console.log(servicePercentPrice);
+console.log('Сумма,с учетом вычета процента подрядчику:', servicePercentPrice);
 
 if (fullPrice >= 40000) {
     console.log('делаем скидку в 10%');
@@ -53,3 +53,11 @@ function getFullPrice() {
 
 let fullProjectPrice = getFullPrice();
 console.log(fullProjectPrice);
+
+
+function getTitle(title) {
+    return title.charAt(0).toUpperCase() + title.slice(1).toLowerCase();
+}
+
+titleProject = getTitle(titleProject);
+console.log(titleProject);
